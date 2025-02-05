f4d089595cad0b689ae3b89c0232fd52e670a940
Oleksandr_Husiev <ohusiev@icloud.com>
2025-02-05 06:39:34 +0100

    updated app logic to be a mock forecast app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,13 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn load_forecast(city_name: &str) -> String {
+    format!("Weather in {} is sunny now!", city_name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![load_forecast])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
