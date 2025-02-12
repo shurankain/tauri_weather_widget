a27e895f5174e3a131dbb28af0c3b84f0c83be80
Oleksandr_Husiev <ohusiev@icloud.com>
2025-02-12 06:58:29 +0100

    logging plugin added to lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
 use tauri::{LogicalSize, Manager};
+use tauri_plugin_log::{Target, TargetKind};
 
 #[derive(Debug, Deserialize)]
 struct Weather {
@@ -63,6 +64,7 @@ async fn get_weather(city: String) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![get_weather])
         .invoke_handler(tauri::generate_handler![save_config])
@@ -73,6 +75,7 @@ pub fn run() {
                 .unwrap();
             Ok(())
         })
+        .plugin(tauri_plugin_log::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
